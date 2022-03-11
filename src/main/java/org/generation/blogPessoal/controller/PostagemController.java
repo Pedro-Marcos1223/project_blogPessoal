43adde658100806179ee5afde7a3f801310c3412
Pedro-Marcos1223 <pedromarcospdm@gmail.com>
2022-03-11 14:41:15 -0300

    @Pedro-Marcos1223 [Deploy]

--- src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -16,46 +16,155 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/postagens")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@Tag(name = "Controller postagem")
 public class PostagemController {
 
 	@Autowired
 	private PostagemRepository repository;
 	
+	@Operation(summary = "Get all posts")
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "List whitch all posts",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Postagem.class))}
+					),
+			@ApiResponse(
+					responseCode = "204",
+					description = "There are no Posts",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseStatusException.class))}
+					),
+			@ApiResponse(
+					responseCode = "500",
+					description = "Internal server error",
+					content = @Content
+					)
+	})	
 	@GetMapping
 	public ResponseEntity<List<Postagem>> GetAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
 	
-	
+	@Operation(summary = "Get post by id")
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Show the post found",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Postagem.class))}
+					),
+			@ApiResponse(
+					responseCode = "404",
+					description = "Post not found",
+					content = @Content
+					),
+			@ApiResponse(
+					responseCode = "500",
+					description = "Internal server error",
+					content = @Content
+					)
+	})
 	@GetMapping("/{id}")
 	public ResponseEntity<Postagem> GetById(@PathVariable long id){
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 	
+	@Operation(summary = "Get post by title")
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Shows posts found with the same keywords",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Postagem.class))}
+					),
+			@ApiResponse(
+					responseCode = "204",
+					description = "There are no posts with this keyword",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseStatusException.class))}
+					),
+			@ApiResponse(
+					responseCode = "500",
+					description = "Internal server error",
+					content = @Content
+					)
+	})
 	
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo ){
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
+	@Operation(summary = "Create a post")
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "201",
+					description = "Create a post",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Postagem.class))}
+					),
+			@ApiResponse(
+					responseCode = "400",
+					description = "Error creating post",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseStatusException.class))}
+					),
+			@ApiResponse(
+					responseCode = "401",
+					description = "Not authorized to create a post",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseStatusException.class))}
+					)
+	})
 	@PostMapping()
 	public ResponseEntity<Postagem> post(@RequestBody  Postagem postagem ){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
 	}
 	
+	@Operation(summary = "Update a post")
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Update the post",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Postagem.class))}
+					),
+			@ApiResponse(
+					responseCode = "400",
+					description = "Error updating post",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseStatusException.class))}
+					),
+			@ApiResponse(
+					responseCode = "401",
+					description = "Not authorized to update a post",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseStatusException.class))}
+					)
+	})
 	@PutMapping()
 	public ResponseEntity<Postagem> put(@RequestBody  Postagem postagem ){
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 	}
 	
+	@Operation(summary = "Update a post")
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Delete the post",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Postagem.class))}
+					),
+			@ApiResponse(
+					responseCode = "401",
+					description = "Unauthorized to delete post",
+					content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseStatusException.class))}
+					)
+	})
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable long id) {
 		repository.deleteById(id);
-	}
-	
-	
+	}	
 }
